3c2ddc9a3b3e5ecc57b073b8e38b341a4742b7e6
zhumo <mozhu888@gmail.com>
2014-06-11 12:57:51 -0400

    refector message model and specs

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,21 +1,21 @@
-class Message < ActiveRecord::Base
+class Message
     SID = ENV['TWILIO_ACCOUNT_SID']
     AUTH_TOKEN = ENV['TWILIO_AUTH_TOKEN']
     TWILIO_PHONE_NUMBER = ENV['TWILIO_PHONE_NUMBER']
 
-    #Text of all the stock message
+    #Text of all the stock messages
     COMMAND_LIST = '"Stop nags" to stop all nags. "Restart nags" to restart nags, if stopped. "Done" to declare the last nag that was sent to you as done. "Remind me to <text>" to create new nag.'
     UNKNOWN_COMMAND_MESSAGE = "Command not recognized. Respond with \"Command list\" for a list of recognized commands"
-    UNKNOWN_USER_MESSAGE = "Sorry, I don't recognize your number. Sign up for The Nagging Machine at www.thenaggingmachine.com"
+    UNKNOWN_USER_MESSAGE = "Sorry, I don't recognize your number. Sign up for The Nagging Machine at the-nagging-machine.herokuapp.com"
     STOP_CONFIRM_MESSAGE = "All nags stopped. Respond with \"Restart nags\" to restart the nagging."
     ALREADY_STOPPED_MESSAGE = "Your nags are already stopped. Respond with \"Restart nags\" to restart the nagging."
     RESTART_CONFIRM_MESSAGE = "Your nags have been restarted. Welcome back!"
     ALREADY_ACTIVE_MESSAGE = "Your nags are already active. Respond with \"Stop nags\" to stop all nags."
     NAG_DONE_MESSAGE = "Good job! I'll stop nagging you about it now."
     PHONE_CONFIRMATION_MESSAGE = "Your phone number has been confirmed. Respond with \"Command list\" to see a list of all text commands you can use."
-    INCORRECT_CONFIRMATION_CODE_MESSAGE = "That code is not recognized. Please refresh the page for a new code and try again. This may have happended because confirmation codes become invalid after one hour." 
+    INCORRECT_CONFIRMATION_CODE_MESSAGE = "That code is not recognized. Please refresh the page for a new code and try again. Confirmation codes become invalid after one hour." 
     WELCOME_MESSAGE = "Welcome to the Nagging Machine! Please enter the 4-digit code you see on your screen."
-    UNCONFIRMED_PHONE_NUMBER_MESSAGE = "Your phone number is unconfirmed. Please visit www.thenaggingmachine.com, log in, and send the 4-digit code found on the screen."
+    UNCONFIRMED_PHONE_NUMBER_MESSAGE = "Your phone number is unconfirmed. Please visit the-nagging-machine.herokuapp.com, log in, and send the 4-digit code found on the screen."
     CREATE_NAG_CONFIRMATION = "OK. I will nag you until you do it!"
   class << self
 
@@ -24,18 +24,20 @@ class Message < ActiveRecord::Base
       message_sender = params[:From]
       user = User.find_by(phone_number: message_sender.sub("+1",""))
 
-      if !User.pluck(:phone_number).include?(message_sender.sub("+1",""))
+      if !user
         reply_body = UNKNOWN_USER_MESSAGE
-      elsif message_body.match(/\A\d{4}\z/) && user.awaiting_confirmation?
-        if user.confirmation_code == message_body && user.confirmation_code_time + 1.hour > Time.now
-          user.confirm_phone_number
-          reply_body = PHONE_CONFIRMATION_MESSAGE
-        else
-          user.generate_confirmation_code
-          reply_body = INCORRECT_CONFIRMATION_CODE_MESSAGE
-        end
       elsif user.awaiting_confirmation?
-        reply_body = UNCONFIRMED_PHONE_NUMBER_MESSAGE
+        if message_body.match(/\A\d{4}\z/)
+          if user.confirmation_code == message_body && user.confirmation_code_time + 1.hour > Time.now
+            user.confirm_phone_number
+            reply_body = PHONE_CONFIRMATION_MESSAGE
+          else
+            user.generate_confirmation_code
+            reply_body = INCORRECT_CONFIRMATION_CODE_MESSAGE
+          end
+        else
+          reply_body = UNCONFIRMED_PHONE_NUMBER_MESSAGE
+        end
       elsif message_body.downcase == "done"
         user.last_ping.declare_done
         reply_body = NAG_DONE_MESSAGE
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,149 +1,172 @@
 require 'spec_helper'
 
 describe Message do
-  describe "route_incoming" do
+  describe ".route_incoming" do
     before(:each) do
       @user = FactoryGirl.create(:user)
       @unconfirmed_user = FactoryGirl.create(:unconfirmed_user)
     end
 
-    it "should send the unknown_user_message if the user's phone number is not registered. This should also change the user's status to active" do
-      expect(Message).to receive(:send_message).with("+10987654321", Message::UNKNOWN_USER_MESSAGE)
-      params = {From: "+10987654321", Body: "test"}
-      Message.route_incoming(params)
+    context "WHEN the incoming message's phone number does not match the phone number of an existing user" do
+      it "IT should send the unknown_user_message if the user's phone number is not registered" do
+        expect(Message).to receive(:send_message).with("+10987654321", Message::UNKNOWN_USER_MESSAGE)
+        params = {From: "+10987654321", Body: "test"}
+        Message.route_incoming(params)
+      end
     end
 
-    it "should send the phone confirmation message if the message is a 4-digit number and the message matches the account's confirmation token and the message is sent within 1 hour of the confirmation code creation. This should make the confirmation code empty" do
-      expect(Message).to receive(:send_message).with(@unconfirmed_user.full_phone_number, Message::PHONE_CONFIRMATION_MESSAGE)
-      params = {From: @unconfirmed_user.full_phone_number, Body: @unconfirmed_user.confirmation_code}
-      Message.route_incoming(params)
+    context "WHEN the incoming message's phone number matches the phone number of an existing user" do
+      context "AND the user is awaiting confirmation" do
+        context "AND the message body consists only of four digits" do
+          context "AND the user's confirmation_code attribute matches the message body AND the confirmation_code_time was less than 1 hour ago" do
+            it "IT should send the phone confirmation message" do
+              expect(Message).to receive(:send_message).with(@unconfirmed_user.full_phone_number, Message::PHONE_CONFIRMATION_MESSAGE)
+              params = {From: @unconfirmed_user.full_phone_number, Body: @unconfirmed_user.confirmation_code}
+              Message.route_incoming(params)
 
-      @unconfirmed_user.reload
+              @unconfirmed_user.reload
 
-      expect(@unconfirmed_user.confirmation_code).to be_nil
-      expect(@unconfirmed_user.confirmation_code_time).to be_nil
-    end
+              expect(@unconfirmed_user.confirmation_code).to be_nil
+              expect(@unconfirmed_user.confirmation_code_time).to be_nil
+            end
+          end
 
-    it "should send the incorrect confirmation code message if the message is a 4-digit number and the message does not match the account's confirmation token and the message is sent within 1 hour of the confirmation code's creation. This should reset the confirmation code" do
-      time = @unconfirmed_user.confirmation_code_time
+          context "AND the user's confirmation_code attributes does not match the message body OR the confirmation_code_time was greater than 1 hour ago" do
+            it "IT should send the incorrect confirmation code message and reset the confirmation code" do
+              time = @unconfirmed_user.confirmation_code_time
 
-      expect(Message).to receive(:send_message).with(@unconfirmed_user.full_phone_number, Message::INCORRECT_CONFIRMATION_CODE_MESSAGE)
-      params = {From: @unconfirmed_user.full_phone_number, Body: "4321"}
-      Message.route_incoming(params)
+              expect(Message).to receive(:send_message).with(@unconfirmed_user.full_phone_number, Message::INCORRECT_CONFIRMATION_CODE_MESSAGE)
+              params = {From: @unconfirmed_user.full_phone_number, Body: "4321"}
+              Message.route_incoming(params)
 
-      @unconfirmed_user.reload
+              @unconfirmed_user.reload
 
-      expect(@unconfirmed_user.confirmation_code).to_not eq("1234")
-      expect(@unconfirmed_user.confirmation_code_time).to_not eq(time)
-    end
+              expect(@unconfirmed_user.confirmation_code).to_not eq("1234")
+              expect(@unconfirmed_user.confirmation_code_time).to_not eq(time)
+            end
+          end
+        end
 
-    it "should send the incorrect confirmation code message if the message is a 4-digit number and the message does match the account's confirmation token and the message is sent more than 1 hour of the confirmation code's creation. This should reset the confirmation code" do
-      time = @unconfirmed_user.confirmation_code_time
-
-      expect(Message).to receive(:send_message).with(@unconfirmed_user.full_phone_number, Message::INCORRECT_CONFIRMATION_CODE_MESSAGE)
-      params = {From: @unconfirmed_user.full_phone_number, Body: "4321"}
-      Message.route_incoming(params)
-
-      @unconfirmed_user.reload
-
-      expect(@unconfirmed_user.confirmation_code).to_not eq("1234")
-      expect(@unconfirmed_user.confirmation_code_time).to_not eq(time)
-    end
-
-    it "should send the unconfirmed phone number message if the user's account is awaiting confirmation and the user sends a message" do
-      expect(Message).to receive(:send_message).with(@unconfirmed_user.full_phone_number, Message::UNCONFIRMED_PHONE_NUMBER_MESSAGE)
-      params = {From: @unconfirmed_user.full_phone_number, Body: "test"}
-      Message.route_incoming(params)
-    end
-
-    it "should send the nag done message and declare the user's last nag to be done if the user sends 'done'" do
-      2.times do |n|
-        FactoryGirl.create(:nag, user: @user)
+        context "AND the message body does not consist of only four digits" do
+          it "IT should send the unconfirmed phone number message if the user's account is awaiting confirmation and the user sends a message" do
+            expect(Message).to receive(:send_message).with(@unconfirmed_user.full_phone_number, Message::UNCONFIRMED_PHONE_NUMBER_MESSAGE)
+            params = {From: @unconfirmed_user.full_phone_number, Body: "test"}
+            Message.route_incoming(params)
+          end
+        end
       end
 
-      nag = FactoryGirl.create(:nag, user: @user, last_ping_time: Time.now + 1.hour)
+      context "AND the user is not awaiting confirmation" do
+        context "AND the message body is 'done'" do
+          it "IT should send the nag done message and declare the user's last nag to be done" do
+            2.times do |n|
+              FactoryGirl.create(:nag, user: @user)
+            end
 
-      expect(Message).to receive(:send_message).with(@user.full_phone_number, Message::NAG_DONE_MESSAGE)
+            nag = FactoryGirl.create(:nag, user: @user, last_ping_time: Time.now + 1.hour)
 
-      params = {From: @user.full_phone_number, Body: "Done"}
-      Message.route_incoming(params)
+            expect(Message).to receive(:send_message).with(@user.full_phone_number, Message::NAG_DONE_MESSAGE)
 
-      nag.reload
+            params = {From: @user.full_phone_number, Body: "Done"}
+            Message.route_incoming(params)
 
-      expect(nag.status).to eq("done")
-    end
+            nag.reload
 
-    it "should send the stop confirm message and stop all nags if the user sends 'stop nags' and the user's current status is active" do
-      @user.update_attributes(status: "active")
+            expect(nag.status).to eq("done")
+          end
+        end
 
-      expect(Message).to receive(:send_message).with(@user.full_phone_number, Message::STOP_CONFIRM_MESSAGE)
-      params = {From: @user.full_phone_number, Body: "Stop nags"}
-      Message.route_incoming(params)
+        context "AND the message body is 'stop nags'" do
+          context "AND the user's status is 'active'" do
+            it "IT should send the stop confirm message and stop all nags" do
+              @user.update_attributes(status: "active")
 
-      @user.reload
+              expect(Message).to receive(:send_message).with(@user.full_phone_number, Message::STOP_CONFIRM_MESSAGE)
+              params = {From: @user.full_phone_number, Body: "Stop nags"}
+              Message.route_incoming(params)
 
-      expect(@user.status).to eq("stopped")
-    end
+              @user.reload
 
-    it "should send the already stopped message if the user sends 'stop nags' while his/her status is stopped" do
-      @user.update_attributes(status: "stopped")
+              expect(@user.status).to eq("stopped")
+            end
+          end
 
-      expect(Message).to receive(:send_message).with(@user.full_phone_number, Message::ALREADY_STOPPED_MESSAGE)
-      params = {From: @user.full_phone_number, Body: "Stop nags"}
-      Message.route_incoming(params)
-    end
+          context "AND the user's status is not 'active'" do
+            it "IT should send the already stopped message" do
+              @user.update_attributes(status: "stopped")
 
-    it "should send the restart confirm message and restart all nags if the user sends 'restart nags' and the user's current status is stopped" do
-      @user.update_attributes(status: "stopped")
+              expect(Message).to receive(:send_message).with(@user.full_phone_number, Message::ALREADY_STOPPED_MESSAGE)
+              params = {From: @user.full_phone_number, Body: "Stop nags"}
+              Message.route_incoming(params)
+            end
+          end
+        end
 
-      expect(Message).to receive(:send_message).with(@user.full_phone_number, Message::RESTART_CONFIRM_MESSAGE)
-      params = {From: @user.full_phone_number, Body: "Restart nags"}
-      Message.route_incoming(params)
-      
-      @user.reload
+        context "AND the message body is 'restart nags'" do
+          context "AND the user's status is 'stopped'" do
+            it "IT should send the restart confirm message and restart all nags" do
+              @user.update_attributes(status: "stopped")
 
-      expect(@user.status).to eq("active")
-    end
+              expect(Message).to receive(:send_message).with(@user.full_phone_number, Message::RESTART_CONFIRM_MESSAGE)
+              params = {From: @user.full_phone_number, Body: "Restart nags"}
+              Message.route_incoming(params)
+              
+              @user.reload
 
-    it "should send the already active message if the user sends 'restart nags' and the user's current status is active" do
-      @user.update_attributes(status: "active")
+              expect(@user.status).to eq("active")
+            end
+          end
 
-      expect(Message).to receive(:send_message).with(@user.full_phone_number, Message::ALREADY_ACTIVE_MESSAGE)
-      params = {From: @user.full_phone_number, Body: "Restart nags"}
-      Message.route_incoming(params)
-    end
+          context "AND the user's status is not 'stopped'" do
+            it "IT should send the already active message" do
+              @user.update_attributes(status: "active")
 
-    it "should send the command list if the user sends 'command list'" do
-      expect(Message).to receive(:send_message).with(@user.full_phone_number, Message::COMMAND_LIST)
-      params = {From: @user.full_phone_number, Body: "Command list"}
-      Message.route_incoming(params)
-    end
+              expect(Message).to receive(:send_message).with(@user.full_phone_number, Message::ALREADY_ACTIVE_MESSAGE)
+              params = {From: @user.full_phone_number, Body: "Restart nags"}
+              Message.route_incoming(params)
+            end
+          end
+        end
 
-    it "should send the create nag confirmation and create a new nag if the user sends 'remind me to <text>'" do
-      expect(Message).to receive(:send_message).with(@user.full_phone_number, Message::CREATE_NAG_CONFIRMATION)
-      params = {From: @user.full_phone_number, Body: "Remind me to take out the trash"}
-      Message.route_incoming(params)
+        context "AND the message body is 'command list'" do
+          it "IT should send the command list if the user sends 'command list'" do
+            expect(Message).to receive(:send_message).with(@user.full_phone_number, Message::COMMAND_LIST)
+            params = {From: @user.full_phone_number, Body: "Command list"}
+            Message.route_incoming(params)
+          end
+        end
 
-      expect(@user.nags.pluck(:contents)).to include("Take out the trash")
-    end
+        context "AND the message body starts with 'remind me to'" do
+          it "IT should send the create nag confirmation and create a new nag if the user sends 'remind me to <text>'" do
+            expect(Message).to receive(:send_message).with(@user.full_phone_number, Message::CREATE_NAG_CONFIRMATION)
+            params = {From: @user.full_phone_number, Body: "Remind me to take out the trash"}
+            Message.route_incoming(params)
 
-    it "should send the unknown command message if the message is an unknown command and the user is in an active state" do
-      expect(Message).to receive(:send_message).with(@user.full_phone_number, Message::UNKNOWN_COMMAND_MESSAGE)
-      params = {From: @user.full_phone_number, Body: "unknown command"}
-      Message.route_incoming(params)
+            expect(@user.nags.pluck(:contents)).to include("Take out the trash")
+          end
+        end
+
+        context "AND the message body is not a recognized command" do
+          it "IT should send the unknown command message if the message is an unknown command and the user is in an active state" do
+            expect(Message).to receive(:send_message).with(@user.full_phone_number, Message::UNKNOWN_COMMAND_MESSAGE)
+            params = {From: @user.full_phone_number, Body: "unknown command"}
+            Message.route_incoming(params)
+          end
+        end
+      end
     end
   end
 
-  describe "#send_welcome_message" do
-    it "should send welcome message" do
+  describe ".send_welcome_message" do
+    it "IT should send welcome message" do
       @user = FactoryGirl.create(:user)
       expect(Message).to receive(:send_message).with(@user.full_phone_number, Message::WELCOME_MESSAGE)
       Message.send_welcome_message(@user)
     end
   end
 
-  describe "#send_nag" do
-    it "should send a message with the nag's owner's phone number and the nag's contents and update the last_ping_time attribute" do
+  describe ".send_nag" do
+    it "IT should send a message with the nag's owner's phone number and the nag's contents and update the last_ping_time attribute" do
       user = FactoryGirl.create(:user)
       nag = FactoryGirl.create(:nag, user: user)
       nag_message = "Remember to #{nag.contents}."
@@ -154,5 +177,4 @@ describe Message do
       expect(nag.last_ping_time.inspect).to eq(Time.now.in_time_zone('UTC').inspect)
     end
   end
-
 end
